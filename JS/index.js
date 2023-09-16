9523d37911ac441fe17e5566149523d34e1dbb97
eduardocd18 <javierecd@gmail.com>
2023-09-16 18:06:14 -0400

    Debes crear el modal para el jueves

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+const open = document.getElementByca('open');
+const modal_container = document.getElementsByClassName('modal');
+const close = document.getElementById('close');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show');
+});
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show');
+    window.location.href = "./tarjeta.html";
+
+});
+
+// determino el tamano de la pantalla   
+var w = window.innerWidth;
+var h = window.innerHeight;
+
+// pinto 250 estrellas random
+for (i = 0; i < 250; i++) {
+    wRan = Math.floor(Math.random() * w);
+    hRan = Math.floor(Math.random() * h);
+    var star = document.createElement("div");
+    star.setAttribute("class", "star");
+    star.style.bottom = hRan + "px";
+    star.style.right = wRan + "px";
+    document.body.appendChild(star);
+}
